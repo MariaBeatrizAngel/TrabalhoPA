7ddc3077a5e8eed1d5355c6a44210b5f49c67525
Maria Beatriz Beatriz <102934524+MariaBeatrizAngel@users.noreply.github.com>
2022-04-04 02:10:05 +0000

    Criando a função Binário para Decimal

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+int bin_to_dec(long long);
+
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+
+  long long bin;
+  int dec;
+
+  printf("Entre com o número binário: ");
+  scanf("%lld", &bin);
+
+  dec = bin_to_dec(bin);
+  printf(" %d", dec);
+}
+
+// function definition
+int bin_to_dec(long long n) {
+  int dec = 0, i = 0, rem;
+
+  while (n!=0) {
+    rem = n % 10;
+    n /= 10;
+    dec += rem * pow(2, i);
+    ++i;
+  }
+
+  return dec;
 }

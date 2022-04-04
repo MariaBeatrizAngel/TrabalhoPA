eaaa129d53fef4df02c87d6a548e24e6cb5106f2
Maria Beatriz Beatriz <102934524+MariaBeatrizAngel@users.noreply.github.com>
2022-04-04 02:32:32 +0000

    Adicionando comentários

--- .ccls-cache/@home@runner@TrabalhoPA/main.c
+++ .ccls-cache/@home@runner@TrabalhoPA/main.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 
+// Declaração das Funções
 long long dec_to_bin(int);
 int bin_to_dec(long long);
 
+
+// ------------------------------
+// MAIN -- ----------------------
+// ------------------------------
 int main(void) {
   long long b1, b2, respb;
   int d1, d2, respd, prog = 1;
   char op;
 
   while(prog == 1){
-    prog = 0;
+    prog = 0; // Reseta variável "Prog"
     printf("Entre com a operação(+. -, /, *) : ");
-    scanf(" %c", &op);
+    scanf(" %c", &op); // Obtem operação
     printf("Entre com o primeiro número binário: ");
-    scanf("%lld", &b1);
+    scanf("%lld", &b1); // Obtem Binário 1
     printf("Entre com o segundo número binário: ");
-    scanf("%lld", &b2);
+    scanf("%lld", &b2); // Obtem Binário 2
 
-    d1 = bin_to_dec(b1);
+    // Converte valores binários para decimáis
+    d1 = bin_to_dec(b1); 
     d2 = bin_to_dec(b2);
 
+    // Switch case da decisão da operação a ser realizada
     switch (op){
       case '+':
         respd = d1 + d2;
@@ -34,19 +41,28 @@ int main(void) {
         respd = d1 * d2;
         break;
     }
-  
+
+    // Converte resposta em decimal para binário
     respb = dec_to_bin(respd);
+    // Coloca a resposta da conta na tela
     printf("\nResposta em binário = %lld \n",respb);
+    
+    // Decisão de continuar as operações
     printf("-------- \n\n");
     printf("Digite 1 para continuar\nOu qualquer tecla para encerrar: ");
     scanf("%d", &prog);
-    system("cls || clear");
+    system("cls || clear"); // Limpa o console
   }
 }
 
+// ------------------------------
+// FUNÇÕES ----------------------
+// ------------------------------
 int bin_to_dec(long long n) {
+  // Função que converte de binário para decimal
   int dec = 0, i = 0, rem;
 
+  // Loop de cálculos de conversão
   while (n!=0) {
     rem = n % 10;
     n /= 10;
@@ -58,9 +74,11 @@ int bin_to_dec(long long n) {
 }
 
 long long dec_to_bin(int n) {
+  // Função que converte de decimal para binário
   long long bin = 0;
   int rem, i = 1;
 
+  // Loop de cálculos de conversão
   while (n!=0) {
     rem = n % 2;
     n /= 2;
